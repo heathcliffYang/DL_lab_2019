13276a8dfbf6f412c93bb15757d04955ac4856e8
heathcliffYang <ginny0922fc2@gmail.com>
2019-03-26 21:44:11 +0800

    Test part finish and separate training

--- Lab1_backpropagation/DLP_LAB1_0756703_楊惟晶/data_generators.py
+++ Lab1_backpropagation/DLP_LAB1_0756703_楊惟晶/data_generators.py
@@ -41,7 +41,7 @@ def show_result(x, y, pred_y, filename):
     plt.subplot(1, 2, 1)
     plt.title('Ground truth', fontsize=18)
     for i in range(x.shape[0]):
-        if y[i] == 0:
+        if y[i] < 0.5:
             plt.plot(x[i][0], x[i][1], 'ro')
         else:
             plt.plot(x[i][0], x[i][1], 'bo')
@@ -49,9 +49,10 @@ def show_result(x, y, pred_y, filename):
     plt.subplot(1, 2, 2)
     plt.title('Predtct result', fontsize=18)
     for i in range(x.shape[0]):
-        if pred_y[i] == 0:
+        if pred_y[i] < 0.5:
             plt.plot(x[i][0], x[i][1], 'ro')
         else:
             plt.plot(x[i][0], x[i][1], 'bo')
 
     plt.savefig(filename)
+    plt.clf()
--- Lab1_backpropagation/DLP_LAB1_0756703_楊惟晶/main.py
+++ Lab1_backpropagation/DLP_LAB1_0756703_楊惟晶/main.py
@@ -6,19 +6,19 @@ import neural_netowrk
 
 # prepare data
 num_of_linear_dots = 100
+X, Y = dg.generate_linear(n=num_of_linear_dots)
 
-x_1, y_1 = dg.generate_linear(n=num_of_linear_dots)
-x_2, y_2 = dg.generate_XOR_easy()
+# x_1, y_1 = dg.generate_linear(n=num_of_linear_dots)
+# x_2, y_2 = dg.generate_XOR_easy()
 
-arr = np.arange(num_of_linear_dots + 21)
-np.random.shuffle(arr)
+# arr = np.arange(num_of_linear_dots + 21)
+# np.random.shuffle(arr)
 
-X = np.vstack((x_1, x_2))
-Y = np.vstack((y_1, y_2))
+# X = np.vstack((x_1, x_2))
+# Y = np.vstack((y_1, y_2))
 
-X = X[arr]
-Y = Y[arr]
-
+# X = X[arr]
+# Y = Y[arr]
 
 # neural network parameters
 Loss_avg = 0
@@ -43,20 +43,23 @@ while True:
         # loss_function
         Loss = net.loss(Y[i], z)
         Loss_avg += Loss[0, 0]
+
+        if (y_pred > 0.5 and Y[i] > 0.5):
+            acc += 1
+        elif (y_pred < 0.5 and Y[i] < 0.5):
+            acc += 1
+
         # back-propagation
         if (Loss[0, 0] > 0.1):
-            # net.peek_weights()
             net.back_propagation(Y[i], y_pred, z, learning_rate=lr)
-            # net.peek_weights()
-            # print("i : ", i, "data : ", X[i], "Loss : ", Loss,
-            #       "Y_pred : ", y_pred, "Y_gt : ", Y[i], "\n")
     epoch += 1
     Loss_avg /= float(X.shape[0])
     loss_pool.append(Loss_avg)
+    acc /= float(X.shape[0])
 
     if (epoch % 100 == 0):
-        print("Epoch ", epoch, " - loss : ", Loss_avg)
-        if (math.fabs(sum(loss_pool)/float(len(loss_pool)) - Loss_avg) < 0.001):
+        print("Epoch ", epoch, " - loss : ", Loss_avg, " - ACC : ", acc)
+        if (math.fabs(sum(loss_pool)/float(len(loss_pool)) - Loss_avg) < 0.001 and Loss_avg < 0.2):
             print("End")
             break
         elif (Loss_avg < 1.):
@@ -64,7 +67,7 @@ while True:
         elif (Loss_avg < 0.5):
             lr = 0.1
         elif (Loss_avg < 0.3):
-            lr = 0.01
+            lr = 0.0001
         elif (math.isnan(Loss_avg)):
             print("Oh no!")
             break
@@ -75,23 +78,85 @@ while True:
         net.peek_weights()
 
 
+X, Y = dg.generate_XOR_easy()
+
+# training
+while True:
+    Loss_avg = 0
+    acc = 0
+    for i in range(X.shape[0]):
+
+        # for i in range(1):
+
+        # forward
+        y_pred, z = net.forward(np.expand_dims(X[i], axis=1))
+        # loss_function
+        Loss = net.loss(Y[i], z)
+        Loss_avg += Loss[0, 0]
+
+        if (y_pred > 0.5 and Y[i] > 0.5):
+            acc += 1
+        elif (y_pred < 0.5 and Y[i] < 0.5):
+            acc += 1
+
+        # back-propagation
+        if (Loss[0, 0] > 0.1):
+            net.back_propagation(Y[i], y_pred, z, learning_rate=lr)
+    epoch += 1
+    Loss_avg /= float(X.shape[0])
+    loss_pool.append(Loss_avg)
+    acc /= float(X.shape[0])
+
+    if (epoch % 100 == 0):
+        print("Epoch ", epoch, " - loss : ", Loss_avg, " - ACC : ", acc)
+        if (math.fabs(sum(loss_pool)/float(len(loss_pool)) - Loss_avg) < 0.001 and Loss_avg < 0.2):
+            print("End")
+            break
+        elif (Loss_avg < 1.):
+            lr = 0.1
+        elif (Loss_avg < 0.5):
+            lr = 0.01
+        elif (Loss_avg < 0.3):
+            lr = 0.001
+        elif (math.isnan(Loss_avg)):
+            print("Oh no!")
+            break
+        # Log per 1000 epochs
+        loss_pool = []
+
+    if (epoch % 1000 == 0):
+        net.peek_weights()
+
 # testing
+print("Linear data")
 x_1, y_1 = dg.generate_linear(n=num_of_linear_dots)
 Y_pred = []
+acc = 0
 for i in range(x_1.shape[0]):
     y_pred, z = net.forward(np.expand_dims(x_1[i], axis=1))
     Y_pred.append(y_pred)
-    print("i : ", i, "- data : ", x_1[i],
-          "Y_pred : ", y_pred, "Y_gt : ", y_1[i])
-
+    if (y_pred > 0.5 and y_1[i] > 0.5):
+        print(i, " - True - Y_pred : ", y_pred, "Y_gt : ", y_1[i])
+        acc += 1
+    elif (y_pred < 0.5 and y_1[i] < 0.5):
+        print(i, " - True - Y_pred : ", y_pred, "Y_gt : ", y_1[i])
+        acc += 1
+print("ACC : ", acc/x_1.shape[0])
 dg.show_result(x_1, y_1, Y_pred, "linear")
 
+
+print("XOR data")
+acc = 0
 x_2, y_2 = dg.generate_XOR_easy()
 Y_pred = []
 for i in range(x_2.shape[0]):
     y_pred, z = net.forward(np.expand_dims(x_2[i], axis=1))
     Y_pred.append(y_pred)
-    print("i : ", i, "- data : ", x_2[i],
-          "Y_pred : ", y_pred, "Y_gt : ", y_2[i])
-
+    if (y_pred > 0.5 and y_2[i] > 0.5):
+        print(i, " - True - Y_pred : ", y_pred, "Y_gt : ", y_2[i])
+        acc += 1
+    elif (y_pred < 0.5 and y_2[i] < 0.5):
+        print(i, " - True - Y_pred : ", y_pred, "Y_gt : ", y_2[i])
+        acc += 1
+print("ACC : ", acc/x_2.shape[0])
 dg.show_result(x_2, y_2, Y_pred, "XOR")
